5581036f38823b2ad216a3b43a3f7c152b8704b0
The5econd <00017917@uca.edu.sv>
2018-09-21 11:23:31 -0600

    PARA ESO EXISTE EL SORT >:V

--- 2.js
+++ 2.js
@@ -1,10 +1,21 @@
-function ordenar(array[]){
-    var array = [5, 6, 7, 9, 3];
-    array.sort();
-}
 
-var x = [ 3, 6, 7, 9, 2, 4]; 
-var lista = [35, 4, 76, 1, 8, 10];
-for(let i = 0; i < lista.length; i++){
-    
+
+var x =['2','', '5', '1'];
+
+function ordernar(){
+    var x =['2','7','8', '5', '1'];
+    var key;
+    var j;
+    for(var x=1; x<x.length;x++){
+        key= x[x];
+        j= x-1;
+        while(j>-1 && key<x[j]){
+            x[j+1]=x[j];
+            j=j-1;
+        }
+        x[j+1]= key;
+
+    }
+    return x;
 }
+console.log(ordernar());
--- 7.js
+++ 7.js
@@ -1,5 +1,5 @@
 var anio = prompt("ingrese un anio");
-if ((anio % 4 == 0 && anio %100 != 0) || anio == 0){
+if ((anio % 4 == 0 && anio %100 != 0) || anio % 400 == 0){
     console.log(`el ${anio} es bisiesto`);
 }else{
     console.log(`el ${anio} no es bisiesto`);
